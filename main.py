d4ab4c286c1f34e8391c2747f56378c555ba192e
Nicolas Lima <nichollaslimma734@gmail.com>
2023-03-20 19:13:18 -0300

    implementado listagem de serviços dos dispositivos e escolha do endereço mac

--- main.py
+++ main.py
@@ -2,38 +2,65 @@ import bluetooth
 import logging
 
 
-
-
 logging.basicConfig(filename="app.log", level=logging.ERROR)
 
+print('\nLista de dispositivos\n')
+devices = bluetooth.discover_devices(lookup_names=True, lookup_class=True)
 
-print('Listagem de dispositivos\n')
-devices = bluetooth.discover_devices(lookup_names = True, lookup_class = True)
+
+
 for addr, name, device_class in devices:
-    print("Device:")
-    print("Device Name: %s" % (name))
-    print("Device MAC Address: %s" % (addr))
-    print("Device Class: %s" % (device_class))
+    print("Dispositivo:")
+    print("Nome do Dispositivo: %s" % (name))
+    print("Endereço MAC do Dispositivo: %s" % (addr))
+    print("Classe do Dispositivo: %s" % (device_class))
     print("\n")
 
-addr = "COLOQUE SEU MAC ADRESS AQUI"
-port = 1
-
-print("\nTentando se conectar ao dispositiVo...")
-
-sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-try:
-    sock.connect((addr, port))
-    message = "Olá, mundo!"
-    sock.send(message)
-    print("\nConexão bem sucedida...\n")
-    sock.close()
-except Exception as error:
-    print("\nNão foi possivel se comunicar com dispositivo...")
-    logging.exception(str(error))
+    services = bluetooth.find_service(address=addr)
+    if len(services) > 0:
+        print(f"Serviços disponíveis do dispositivo {name}:")
+        for svc in services:
+            print(f"\tNome do Serviço: {svc['name']}")
+            print(f"\t    Host: {svc['host']}")
+            print(f"\t    Description: {svc['description']}")
+            print(f"\t    Provided by: {svc['provider']}")
+            print(f"\t    Protocol: {svc['protocol']}")
+            print(f"\t    Service classes: {svc['service-classes']}")
+            print(f"\t    Profiles: {svc['profiles']}\n")
 
 
 
+while True:
+
+    print("\nQual o endereço MAC que deseja conectar ?")
+    addr = str(input(': '))
+    port = 3
+
+    if bluetooth.is_valid_address(addr):
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        sock.settimeout(15.0)
+
+        print("\nTentando se conectar ao dispositivo...\n")
+
+        try:
+            sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+            sock.connect((addr, port))
+            conexao = sock.getpeername()
+            print("Conexão estabelecida com sucesso!\n")
+            print(conexao)
+            response = sock.send("Hello World")
+            print(response)
+            sock.close()
+            break
+
+        except Exception as error:
+            print(f"Não foi possivel se conectar ao dispositivo {addr}")
+            logging.exception(str(error))
+            print("n\Erro armazenado no arquivo de log....\n")
+            break
+
+    else: 
+        print(f"Endereço mac {addr} não é valido")
 
 
 # O PyBluez suporta os seguintes protocolos Bluetooth:
